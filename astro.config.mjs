285004f639bcd681a0a59b7afbdaed6eb41a5897
Marcel Kaiser <marcel.kaiser@janitza.de>
2026-08-04 15:07:43 +0200

    fix: make assets work on project Pages URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://kaiser-marcel.de',
   output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
